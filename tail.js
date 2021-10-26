4f5c33e67a8403f26f018b4d5129028a570e1832
rom <mozzieisme@gmail.com>
2021-10-26 00:48:33 -0400

    Modification of tail.js file

--- tail.js
+++ tail.js
@@ -1,17 +1,32 @@
 // FUNCTION IMPLEMENTATION
 
+let newResult = "";
+let newExpected = "";
+
+
 const tail = function(arr) {
   return arr.slice(1);
 };
 
 
 const assertEqual = function(tail, expected) {
-  if (tail === expected) {
-    // console.log(`✅✅✅ Assertion Passed: ${tail} === ${expected}`);
+  for (let x = 0; x < result.length; x++) {
+    if (result[x] === expected[x]) {
+      newResult += result[x];
+      newResult += " ";
+
+      newExpected += expected[x];
+      newExpected += " ";
+    }
+  }
+
+  if (newResult === newExpected) {
+    console.log(`✅✅✅ Assertion Passed: ${tail} === ${expected}`);
   } else {
-    // console.log(`🛑🛑🛑 Assertion Failed: ${tail} !== ${expected}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${tail} !== ${expected}`);
   }
 };
+
 
 
 
@@ -22,7 +37,7 @@ assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
 
 
 
-
-for (const iterator of result) {
-  console.log(iterator);
-}
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
