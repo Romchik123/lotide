9226b10c58a1ffafdf78d1aea45390ab213187ce
rom <mozzieisme@gmail.com>
2021-10-27 19:20:29 -0400

    Refactoring

--- middle.js
+++ middle.js
@@ -1,28 +1,82 @@
-const middle = function(arr) {
-  let result = [];
-  let i = 0;
-
-  if (arr.length === 1 || arr.length === 2) {
-    result = [];
-  } else if (arr.length % 2 !== 0) {
-    i = Math.floor(arr.length / 2);
-    result.push(arr[i]);
-  } else if (arr.length % 2 === 0) {
-    i = Math.floor(arr.length / 2);
-    result.push(arr[i - 1]);
-    result.push(arr[i]);
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
   }
-
-  console.log(result);
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 
 
 
 
-middle([1]); // => []
-middle([1, 2]); // => []
-middle([1, 2, 3]); // => [2]
-middle([1, 2, 3, 4, 5]); // => [3]
-middle([1, 2, 3, 4]); // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
+// console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+// console.log(eqArrays([1, 2, 3], [3, 2, 1]));// => false
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+
+
+
+
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+};
+
+
+
+
+
+// assertArraysEqual([1, 2, 3], [1, 2, 3])
+// assertArraysEqual([1, 2, 3], [3, 2, 1])
+// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"])
+// assertArraysEqual(["1", "2", "3"], ["1", "2", 3])
+
+
+
+
+
+const middle = function (arr) {
+  let result = [];
+  let index = Math.floor(arr.length / 2);
+
+  if (arr.length <= 2) {
+    result = [];
+  } else if (arr.length % 2 === 1) {
+    result.push(arr[index]);
+  } else if (arr.length % 2 === 0) {
+    result.push(arr[index - 1]);
+    result.push(arr[index]);
+  }
+  return result;
+};
+
+
+
+
+
+// console.log(middle([1])) // => []
+// console.log(middle([1, 2])) // => []
+// console.log(middle([1, 2, 3]))  // => [2]
+// console.log(middle([1, 2, 3, 4, 5])) // => [3]
+// console.log(middle([1, 2, 3, 4])) // => [2, 3]
+// console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+
+
+
+
+
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
