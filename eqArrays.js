7ff613f44c3d5be8bcc378a2685090297a5a8ab0
rom <mozzieisme@gmail.com>
2021-10-27 00:21:49 -0400

    Add new file

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+const eqArrays = function(arr1, arr2) {
+  let result = [];
+
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        result.push(false);
+      } else if (arr1[i] === arr2[i]) {
+        result.push(true);
+      }
+    }
+
+    if (result.includes(false)) {
+      console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+    } else {
+      console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+    }
+  }
+};
+
+
+
+
+
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
