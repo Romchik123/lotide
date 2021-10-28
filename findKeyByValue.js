d231d19c7fe6121defa84d08e953803293c804f4
rom <mozzieisme@gmail.com>
2021-10-28 03:51:34 -0400

    Add new file

---
+++ findKeyByValue.js
@@ -0,0 +1,48 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+
+
+const findKeyByValue = function(bestTVShowsByGenre, params2) {
+  let result = "";
+
+  for (const key in bestTVShowsByGenre) {
+    if (bestTVShowsByGenre[key] === params2) {
+      result = key;
+    } else {
+      result = undefined;
+    }
+  }
+  return result;
+};
+
+
+
+
+
+
+
+
+
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+
+
+
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
