a6934ae4ecf066472f2d486dc1ef857402ae4155
rom <mozzieisme@gmail.com>
2021-10-31 21:36:55 -0400

    Add file findKey.js

---
+++ findKey.js
@@ -0,0 +1,52 @@
+const assertEqual = function(arr1, arr2) {
+  if (arr1 === arr2) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+
+
+
+
+const findKey = function(bestTVShowsByGenre, params2) {
+  let result = undefined;
+
+  for (const key in bestTVShowsByGenre) {
+    if (params2(bestTVShowsByGenre[key])) {
+      result = key;
+      break;
+    }
+  }
+  return result;
+};
+
+
+
+
+
+// findKey({
+//   "Blue Hill": { stars: 1 },
+//   "Akaleri":   { stars: 3 },
+//   "noma":      { stars: 2 },
+//   "elBulli":   { stars: 3 },
+//   "Ora":       { stars: 2 },
+//   "Akelarre":  { stars: 3 }
+// }, x => x.stars === 2); // => "noma"
+
+
+
+
+
+assertEqual(
+  findKey({
+    "Blue Hill": { stars: 1 },
+    "Akaleri":   { stars: 3 },
+    "noma":      { stars: 2 },
+    "elBulli":   { stars: 3 },
+    "Ora":       { stars: 2 },
+    "Akelarre":  { stars: 3 }
+  }, x => x.stars === 2),
+  "noma"
+);
